d9b389993af5e5fa7ba4daed6fc8da233638dc4e
PBur27 <p.burda2003@gmail.com>
2024-12-07 23:39:21 +0100

    learn dynamic loading of cards for asset category card bar

--- asset-trader/src/MainPage.jsx
+++ asset-trader/src/MainPage.jsx
@@ -1,6 +1,5 @@
 import '../stylesheets/custom.scss'
 import TitleBar from "./components/TitleBar"
-import MainPageFilter from './components/MainPageFilter'
 import { Container } from 'react-bootstrap'
 
 function MainPage() {
@@ -10,12 +9,9 @@ function MainPage() {
       <header>
         <TitleBar></TitleBar>
       </header>
-      <div>
-        <div className=' m-auto fs-1 w-50 bg-primary'>
-          TEXTEXAMPLE
-        </div>
-      </div>
+      <Container>
 
+      </Container>
 
     </>
   )
